18dacde72490c20586a7be3e0a30aa336c9c1376
weiwang <2424947384@qq.com>
2019-08-01 22:06:47 +0800

    修改点赞数据库删除定时任务时间

--- app/schedule/updateLikeInfo.js
+++ app/schedule/updateLikeInfo.js
@@ -4,11 +4,7 @@ class UpdateCache extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      cron: '*/1 *',
-      cronOptions: {
-        currentDate: '2019-07-31 23:59:59',
-        // utc: 'UTC'
-      },
+      cron: '0 0 0 1 * *',
       type: 'all', // 指定所有的 worker 都需要执行
     };
   }
